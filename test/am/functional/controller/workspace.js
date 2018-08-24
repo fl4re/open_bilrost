f1d8140a1058da124c88c1d4ed6e417a39f95a48
Maxime Helen <maxime.helen@starbreeze.com>
2018-08-24 15:37:16 -0700

    Consolidate test by checking if empty directory get removed

--- test/am/functional/controller/workspace.js
+++ test/am/functional/controller/workspace.js
@@ -220,6 +220,7 @@ describe('Run Workspace related functional tests for the API', function () {
         it('Reset a workspace', function (done) {
             this.timeout(8*this.timeout());
             fs.writeFileSync(path.join(test_util.get_carol_path(), 'test'), 'Hello world!');
+            fs.outputFileSync(path.join(test_util.get_carol_path(), 'foo', 'bar'), 'Hello world!');
             test_util.client
                 .post(`/assetmanager/workspaces/${encodeURIComponent(test_util.get_carol_file_uri())}/reset`)
                 .send()
