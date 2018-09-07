49d3e04aa88eafe8c4bb8bb24499f6eac55ef791
Maxime Helen <maxime.helen@starbreeze.com>
2018-09-07 00:37:10 -0700

    Clean status manager

--- assetmanager/status_manager.js
+++ assetmanager/status_manager.js
@@ -29,65 +29,45 @@ function Status_manager (workspace) {
 
     this.general_status = new General_status('general');
 
-    this.get_statuses = () => {
-        return this.status;
-    };
+    this.get_statuses = () => this.status;
 
-    this.get_status = ref => {
-        return this.get_statuses().find(status => status.get_ref() === ref);
-    };
+    this.get_status = ref => this.get_statuses().find(status => status.get_ref() === ref);
 
-    this.update_and_retrieve_status = () => {
-        return this.sync_all_status_from_database().then(() => this.get_statuses());
-    };
+    this.update_and_retrieve_status = () => this.sync_all_status_from_database().then(() => this.get_statuses());
 
-    this.sync_all_status_from_database = () => {
-        return Promise.all(this.get_statuses().map(status => status.sync_from_database()));
-    };
+    this.sync_all_status_from_database = () => Promise.all(this.get_statuses().map(status => status.sync_from_database()));
 
-    this.get_general_status = () => {
-        return this.sync_all_status_from_database()
-            .then(() => {
-                this.general_status.set_state(status_config.integrity.VALID);
-                let result_priority = map_states_to_priorities[this.general_status.get_state()];
-                this.get_statuses()
-                    .forEach((item, index, array) => {
-                        let current_priority = map_states_to_priorities[item.get_state()];
-                        if (current_priority > result_priority) {
-                            this.general_status.set_state(item.get_state());
-                            this.general_status.set_info('guilty_ref', item.get_ref());
-                            this.general_status.set_info('guilty_info', item.get_info());
-                            result_priority = current_priority;
-                        }
-                    });
-                return this.general_status;
-            });
-    };
+    this.get_general_status = () => this.sync_all_status_from_database()
+        .then(() => {
+            this.general_status.set_state(status_config.integrity.VALID);
+            let result_priority = map_states_to_priorities[this.general_status.get_state()];
+            this.get_statuses()
+                .forEach((item, index, array) => {
+                    let current_priority = map_states_to_priorities[item.get_state()];
+                    if (current_priority > result_priority) {
+                        this.general_status.set_state(item.get_state());
+                        this.general_status.set_info('guilty_ref', item.get_ref());
+                        this.general_status.set_info('guilty_info', item.get_info());
+                        result_priority = current_priority;
+                    }
+                });
+            return this.general_status;
+        });
 
     this.run_all_status = () => Promise.all(this.get_statuses().map(status => status.run()));
 
-    this.check_overall_validation = () => {
-        return this.asset_repo_manager.validate_vcs()
-            .then(() => this.sync_all_status_from_database())
-            .then(() => {
-                let workspace = this.get_status("workspace_validator");
-                return this.run_all_status()
-                    .then(() => {
-                        let invalid_status_list = this.get_statuses()
-                            .filter(status => status.get_state() !== status_config.integrity.VALID);
-                        if (!invalid_status_list.length) {
-                            return workspace.save_status()
-                                .then(() => this);
-                        } else {
-                            this.error = errors.INTERNALERROR(JSON.stringify(invalid_status_list.map(status => status.status.info)));
-                            throw this;
-                        }
-                    });
-            });
-    };
+    this.check_overall_validation = () => this.asset_repo_manager.validate_vcs()
+        .then(() => this.sync_all_status_from_database())
+        .then(() => this.run_all_status())
+        .then(() => {
+            const invalid_status_list = this.get_statuses()
+                .filter(status => status.get_state() !== status_config.integrity.VALID);
+            if (invalid_status_list.length) {
+                throw errors.INTERNALERROR(JSON.stringify(invalid_status_list.map(status => status.status.info)));
+            }
+        });
 
     return this;
-
 }
 
 module.exports = Status_manager;
