5fbf2651a858210f72c98dd92add4b7b8eb56c26
maxime-helen-sb <maxime.helen@starbreeze.com>
2018-10-25 18:22:38 -0700

    Extract resource controller to its own module

--- controllers/asset.js
+++ controllers/asset.js
@@ -10,13 +10,6 @@ const _url = require('url');
 const _asset = require('../assetmanager/asset');
 const create_handler = require('../lib/handler');
 
-const assets_regexp = /^\/assetmanager\/workspaces\/([^/]*)(\/assets\/.*)/;
-const assets_rename_regexp = /^\/assetmanager\/workspaces\/([^/]*)\/rename(\/assets\/.*)/;
-const workspaces_assets_regexp = /^\/contentbrowser\/workspaces\/([^/]*)(\/assets\/.*)/;
-const workspaces_resources_regexp = /^\/contentbrowser\/workspaces\/([^/]*)(\/resources\/.*)/;
-
-const errors = require('../lib/errors')('asset manager');
-
 const sanitize = function(query_argument) {
     if (query_argument === undefined) {
         query_argument = '';
@@ -39,7 +32,7 @@ module.exports = function(server, context) {
     const _workspace = require('../assetmanager/workspace')(context);
 
     // WORKSPACES
-    server.get(workspaces_assets_regexp, function(req, res, next) {
+    server.get(/^\/contentbrowser\/workspaces\/([^/]*)(\/assets\/.*)/, function(req, res, next) {
         const handler = create_handler(req, res, next);
         let workspace_identifier = decodeURIComponent(req.params[0]);
         let asset_ref = decodeURIComponent(req.params[1]);
@@ -63,40 +56,7 @@ module.exports = function(server, context) {
             }).catch(err => handler.handleError(err));
     });
 
-    server.get(workspaces_resources_regexp, function(req, res, next) {
-        let workspace_identifier = decodeURIComponent(req.params[0]);
-        let ref = decodeURIComponent(req.params[1]);
-
-        let handler = create_handler(req, res, next);
-        if (ref.split('.bilrost').length > 1) {
-            handler.handleError(errors('Asset').RESTRICTED('.bilrost directory'));
-        }
-        _workspace.find(workspace_identifier)
-            .then(function(workspace) {
-                let options = {
-                    filterName: req.query.uri,
-                    maxResults: parseInt(req.query.maxResults, 10),
-                    start: parseInt(req.query.start, 10),
-                    q: req.query.q
-                };
-                return workspace.resource.get(ref, options)
-                    .then(function(resource) {
-                        let output = resource.output;
-                        if (resource.indexOfMoreResults) {
-                            output.nextLink = get_next_url(req.url, resource.indexOfMoreResults);
-                        }
-                        if (output.header) {
-                            for (let i = 0, keys = Object.keys(output.header); i<keys.length; i++) {
-                                res.header(keys[i], output.header[keys[i]]);
-                            }
-                            delete output.header;
-                        }
-                        handler.sendJSON(output, 200);
-                    });
-            }).catch(err => handler.handleError(err));
-    });
-
-    server.put(assets_regexp, async (req, res, next) => {
+    server.put(/^\/assetmanager\/workspaces\/([^/]*)(\/assets\/.*)/, async (req, res, next) => {
         const handler = create_handler(req, res, next);
         const workspace_identifier = sanitize(req.params[0]);
         const asset_ref = sanitize(req.params[1]);
@@ -121,7 +81,7 @@ module.exports = function(server, context) {
         }
     });
 
-    server.post(assets_rename_regexp, async (req, res, next) => {
+    server.post(/^\/assetmanager\/workspaces\/([^/]*)\/rename(\/assets\/.*)/, async (req, res, next) => {
         const handler = create_handler(req, res, next);
         const workspace_identifier = sanitize(req.params[0]);
         const asset_ref = sanitize(req.params[1]);
@@ -138,7 +98,7 @@ module.exports = function(server, context) {
         }
     });
 
-    server.del(assets_regexp, async (req, res, next) => {
+    server.del(/^\/assetmanager\/workspaces\/([^/]*)(\/assets\/.*)/, async (req, res, next) => {
         const handler = create_handler(req, res, next);
         const workspace_identifier = sanitize(req.params[0]);
         const asset_ref = sanitize(req.params[1]);
